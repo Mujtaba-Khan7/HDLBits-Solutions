7240c20270561081e39a086b5db61d1b3973d9b4
MUJTABA <127471852+Stockholm-gamer@users.noreply.github.com>
2026-04-14 19:10:36 +0530

    Update chip7458.v

--- 02_Verilog_Language/1_Basics/chip7458.v
+++ 02_Verilog_Language/1_Basics/chip7458.v
@@ -7,5 +7,5 @@ module top_module (
 wire one = p1a & p1b & p1c , two = p1d & p1e & p1f;
 wire three = p2a & p2b , four = p2c & p2d;
 assign p1y = one | two;
-assign p2y = three | four;
+assign p2y = three | four; // I could also have directly done the assignement part without defining any wire
 endmodule
