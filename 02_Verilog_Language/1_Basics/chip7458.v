4b052163f94dea366cca18233777b202032dc6d5
MUJTABA <127471852+Stockholm-gamer@users.noreply.github.com>
2026-04-14 19:05:52 +0530

    Create chip7458.v

---
+++ 02_Verilog_Language/1_Basics/chip7458.v
@@ -0,0 +1,11 @@
+`default_nettype none
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+wire one = p1a & p1b & p1c , two = p1d & p1e & p1f;
+wire three = p2a & p2b , four = p2c & p2d;
+assign p1y = one | two;
+assign p2y = three | four;
+endmodule
