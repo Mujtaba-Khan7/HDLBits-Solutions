4519b8bcf971b423f41937047a83240d418cc4d8
Mujtaba <127471852+Mujtaba-Khan7@users.noreply.github.com>
2026-04-16 10:32:56 +0530

    Create vector3.v

---
+++ 02_Verilog_Language/2.Vectors/vector3.v
@@ -0,0 +1,10 @@
+`default_nettype none
+module top_module (
+    input [4:0] a, b, c, d, e, f,
+    output [7:0] w, x, y, z );
+  // Each concatenation must sum to 8 bits
+    assign w = {a[4:0],b[4:2]};
+    assign x = {b[1:0],c[4:0],d[4]};
+    assign y = {d[3:0],e[4:1]};
+    assign z = {e[0],f[4:0],2'b11};
+endmodule
