82270690be84ea463d03f0b81fb157f5e2e93c3f
Mujtaba <127471852+Mujtaba-Khan7@users.noreply.github.com>
2026-05-02 14:21:24 +0530

    Create Reduction_operators.v

---
+++ 02_Verilog_Language/5_More_verilog_features/Reduction_operators.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output out_and,
+    output out_or,
+    output out_xor 
+);
+  assign  out_and = &in; // in[0]&in[1]&in[2]&......&in[99]
+  assign 	out_or =  |in; // in[0]|in[1]|in[2]|......|in[99]
+  assign 	out_xor = ^in; // in[0]^in[1]^in[2]^......^in[99]
+  
+endmodule
