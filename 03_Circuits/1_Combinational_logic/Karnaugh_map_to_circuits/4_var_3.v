0e8aaf2ea5639eebad142d8fe47675a9d4e65bc0
Mujtaba <127471852+Mujtaba-Khan7@users.noreply.github.com>
2026-05-06 23:00:00 +0530

    Create 4_var_3.v

---
+++ 03_Circuits/1_Combinational_logic/Karnaugh_map_to_circuits/4_var_3.v
@@ -0,0 +1,10 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    //SOP
+    assign out = a&~b&~c&~d | ~a&b&~c&~d | ~a&~b&~c&d | ~a&~b&c&~d | ~a&b&c&d | a&~b&c&d | a&b&~c&d | a&b&c&~d ;
+
+endmodule
