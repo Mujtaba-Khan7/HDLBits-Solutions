ed3a07294af2db9f71da955a2e36e3f15b4a32e5
Mujtaba <127471852+Mujtaba-Khan7@users.noreply.github.com>
2026-06-24 16:26:22 +0530

    Create slow_decade_counter.v

---
+++ 03_Circuits/2_Sequential_logic/Counters/slow_decade_counter.v
@@ -0,0 +1,20 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+always @(posedge clk) begin
+        if (reset) begin
+            q <= 4'b0000;        // Reset to 0
+        end
+   else if (slowena) begin
+            if (q == 4'd9) begin
+                q <= 4'b0000; // Wrap around to 0 after reaching 9
+            end else begin
+                q <= q + 1'b1;
+            end
+        end
+    // If slowena is low, q keeps its current value automatically
+    end
+endmodule
